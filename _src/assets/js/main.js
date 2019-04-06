367241b439e458708959921dbad376f8e632ee13
antonellam <antonella.michelli@gmail.com>
2019-04-07 00:33:39 +0200

    Improved SCSS

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+function sortSelectOptions(selectElement) {
+	var options = $(selectElement);
+
+	options.sort(function(a,b) {
+		if (a.text.toUpperCase() > b.text.toUpperCase()) return 1;
+		else if (a.text.toUpperCase() < b.text.toUpperCase()) return -1;
+		else return 0;
+	});
+
+	$(selectElement).empty().append( options );
+}
