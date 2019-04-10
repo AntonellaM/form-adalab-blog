81b67b8cd69508ff83c9739180fd6ccb212e3532
antonellam <antonella.michelli@gmail.com>
2019-04-10 12:19:46 +0200

    Commit

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,15 +1,3 @@
 'use strict';
 
 console.log('>> Ready :)');
-
-function sortSelectOptions(selectElement) {
-	var options = $(selectElement);
-
-	options.sort(function(a,b) {
-		if (a.text.toUpperCase() > b.text.toUpperCase()) return 1;
-		else if (a.text.toUpperCase() < b.text.toUpperCase()) return -1;
-		else return 0;
-	});
-
-	$(selectElement).empty().append( options );
-}
