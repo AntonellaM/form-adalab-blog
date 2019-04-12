539370f401087639af9b0a7de0ec17ef9b909e9d
antonellam <antonella.michelli@gmail.com>
2019-04-12 12:39:26 +0200

    Finish form style

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const listEls = document.querySelectorAll('.list-item');
+console.log(listEls);
+
+function listClickHandler(event) {
+    event.currentTarget.classList.toggle('list-selected');
+}
+
+for (let i = 0; i < listEls.length; i++) {
+    listEls[i].addEventListener('click', listClickHandler);
+}
